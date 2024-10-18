4cdbfb3f61e9657cb516231f17eded78e4dfe5ca
ajouatom <ajouatom@gmail.com>
2024-10-18 21:42:21 +0900

    fix...

--- opendbc_repo/opendbc/car/hyundai/carcontroller.py
+++ opendbc_repo/opendbc/car/hyundai/carcontroller.py
@@ -400,10 +400,10 @@ class CarController(CarControllerBase):
     send_button_allowed = (self.frame - self.last_button_frame) > self.button_wait
     #CC.debugTextCC = "{} speed_diff={:.1f},{:.0f}/{:.0f}, button={}, button_wait={}, count={}".format(
     #  send_button_allowed, speed_diff, target, current, send_button, self.button_wait, self.button_spamming_count)
-    debugTextCC = "{} speed_diff={:.1f},{:.0f}/{:.0f}, button={}, button_wait={}, count={}".format(
-       send_button_allowed, speed_diff, target, current, send_button, self.button_wait, self.button_spamming_count)
-    print(debugTextCC)
-    print(f"frame={self.frame} - {self.last_button_frame} = {self.frame - self.last_button_frame}")
+    #debugTextCC = "{} speed_diff={:.1f},{:.0f}/{:.0f}, button={}, button_wait={}, count={}".format(
+    #   send_button_allowed, speed_diff, target, current, send_button, self.button_wait, self.button_spamming_count)
+    #print(debugTextCC)
+    #print(f"frame={self.frame} - {self.last_button_frame} = {self.frame - self.last_button_frame}")
 
     if send_button_allowed or activate_cruise:
       self.button_spamming_count = self.button_spamming_count + 1 if send_button == Buttons.RES_ACCEL else self.button_spamming_count - 1
